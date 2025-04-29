06151d1a7493050cdd17b44ce58217a9fa714c3d
karlo-kasi <karlokasi1995@gmail.com>
2025-04-29 16:45:09 +0200

    snack 8

--- main.js
+++ main.js
@@ -170,3 +170,11 @@ function contoAllaRovescia(n, messaggio) {
 }
 
 console.log(contoAllaRovescia(10, "Fine conto alla rovescia!"))
+
+
+/*🎯 Snack 9 (Bonus)
+Creare una funzione che esegue una sequenza di operazioni con ritardi
+Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+
+Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.*/
+
