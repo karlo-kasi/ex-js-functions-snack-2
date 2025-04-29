18a5bfcb048bbdf1ccb4f547a53f8ea9eeffcf29
karlo-kasi <karlokasi1995@gmail.com>
2025-04-29 14:15:46 +0200

    fix

--- main.js
+++ main.js
@@ -57,3 +57,9 @@ function creaTimer(tempo) {
 const timer3s = creaTimer(4000)
 
 timer3s()
+
+/*🏆 Snack 5
+Crea una funzione stampaOgniSecondo con setInterval.
+Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.*/
